09f4f0734cf202e72fc27a06f81c518237f2240b
ziroyamada1192 <83441232+ziroyamada1192@users.noreply.github.com>
2021-05-15 19:14:09 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+  function draw() {
+    const canvas = document.querySelector('canvas');
+    if (typeof canvas.getContext === 'undefined') {
+      return;
+    }
+    const ctx = canvas.getContext('2d');
+     const g = ctx.createRadialGradient(
+      canvas.width /2, canvas.height /2, 50,
+      canvas.width /2 + 100, canvas.height /2 + 200, 500
+      );
+    
+    g.addColorStop(0, '#f00');
+    g.addColorStop(0.1, '#0f0');
+    g.addColorStop(1, '#00f');
+    
+    ctx.fillStyle = g;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  draw();
+}
